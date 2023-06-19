5e67560d09f11955302f569a6dc6a85175e9210d
applesoft <risingsoft1@gmail.com>
2023-06-20 02:20:11 +0300

    init

---
+++ src/webparts/fap/components/IFapProps.ts
@@ -0,0 +1,8 @@
+export interface IFapProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+  test: boolean;
+}
---
+++ src/webparts/fap/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IFapWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'FapWebPartStrings' {
+  const strings: IFapWebPartStrings;
+  export = strings;
+}
